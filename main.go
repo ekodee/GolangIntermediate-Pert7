587971fc55a8b5ea57813d96c75002c12a6fd7f5
ekodee <msafiiekokuncoro1@gmail.com>
2025-07-10 22:22:05 +0700

    act golang intermediate lepkom pertemuan7

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"pertemuan7_10122817/configs"     //sesuaikan nama folder
+	"pertemuan7_10122817/handlers"    //sesuaikan nama folder
+	"pertemuan7_10122817/middlewares" //sesuaikan nama folder
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	PORT := 3817 //10122817
+
+	configs.ConnectDB()
+	if configs.DB == nil {
+		log.Fatal("Database connection failed")
+	}
+	defer func() {
+		if err := configs.DB.Close(); err != nil {
+			log.Printf("Error closing database connection: %v", err)
+		}
+	}()
+
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("catalog"))
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		handlers.ServeStaticFile(w, r, "catalog", fileServer)
+	})
+	mux.HandleFunc("/api/games/", handlers.HandleGames)
+	mux.HandleFunc("/api/games", handlers.HandleGames)
+
+	// Apply middleware
+	loggedMux := middlewares.LogRequestHandler(mux)
+
+	//Todo : Jalankan HTTP server di golang
+	fmt.Printf("Server berjalan di http://localhost:%d\n", PORT)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", PORT), loggedMux))
+}
